3d7343c25c56650678a37f258669307c5dc42f33
Cheattos <rrqlemin@gmail.com>
2024-05-15 11:30:33 +0700

    revisi image

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -8,7 +8,7 @@ export default function Footer() {
             <Fragment>
                 <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
                     <div className="relative flex h-16 items-center justify-between">
-                        <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+                        <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-center">
                             <div className="flex flex-shrink-0 items-center">
                             <p className="text-white">Copyright &copy; {currentYear} lezzform.com</p>
                             </div>
